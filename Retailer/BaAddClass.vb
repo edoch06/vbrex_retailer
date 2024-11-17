cd2492f281274c37a3b6db04ea45cdf57511b805
orca <edoch06@gmail.com>
2024-11-17 15:56:29 +0300

    ba add class

---
+++ Retailer/BaAddClass.vb
@@ -0,0 +1,20 @@
+﻿Imports System
+Imports System.Data
+Imports System.Data.Common
+Imports System.Data.Odbc
+Imports System.Data.SqlClient
+Imports System.Drawing.Printing
+Imports System.IO
+Imports System.Text
+Imports System.Media
+Public Class BaAddClass
+    Private Const WM_SYSCOMMAND As Integer = &H112
+    Private Const SC_MAXIMIZE As Integer = &HF030
+    Private Const WM_NCLBUTTONDBLCLK As Integer = &HA3
+    Private Const HTCAPTION As Integer = &H2
+    Private widthint As Integer = 714
+    Private heightint As Integer = 349
+    '714, 349
+
+  
+End Class
